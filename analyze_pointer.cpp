b448c378c68343c7f99efd5245c5f46c175b4ba6
ufatima-se <uuach123@gmail.com>
2026-09-18 21:11:29 +0500

    Adding comments to task 3

--- analyze_pointer.cpp
+++ analyze_pointer.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// Prints the pointer's memory address and the value it points to
 void analyze_pointer(int* ptr) {
     cout << "Address: " << ptr << endl;
-    cout << "Value: " << *ptr << endl;
+    cout << "Value: " << *ptr << endl; // Dereferences pointer to get value
 }
- int main() {
-    
+
+int main() {
+    // Stack allocation
     int iValue = 10;
     cout << "Stack variable: " << endl;
-    analyze_pointer(&iValue);
+    analyze_pointer(&iValue); // Pass address of stack variable
 
-
+    // Heap allocation
     int* heapValue = new int[100];
     cout << "Heap variable: " << endl;
-    analyze_pointer(heapValue);
+    analyze_pointer(heapValue); // Pass pointer to heap memory
 
- return 0;
- }
-
- 
+    delete[] heapValue; // Deallocate heap memory
+    return 0;
+}
