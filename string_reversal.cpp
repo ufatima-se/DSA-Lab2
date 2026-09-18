f0094dce528b20adde47402510e5eabff0c7898c
ufatima-se <uuach123@gmail.com>
2026-09-18 11:17:35 +0500

    Adding task 4 (string_reversal)

---
+++ string_reversal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void reverse_str(char* str,int size) {
+    int left = 0;
+    int right = size - 1;
+    
+    while(left <= right) {
+        char temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+int main() {
+
+    int size;
+    cout << "Enter size of string: ";
+    cin >> size;
+
+    char* str = new char[size];
+
+    cout << "Enter a string to reverse: " ;
+    cin >> str;
+
+    reverse_str(str,size);
+
+    cout << "The reversed string is: " << str << endl;
+
+    return 0;
+ }
+
+ 
+
+ 
+
+ 
+
